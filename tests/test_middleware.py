aeaff8493d81bd5198d68501bd6131af42fd9056
Gustavo Narea <me@gustavonarea.net>
2010-01-25 12:30:53 +0000

    Added middleware which implements the wsgiorg.routing_args standard. Fixes #1.

---
+++ tests/test_middleware.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for the Django middleware.
+
+"""
+from nose.tools import eq_, ok_
+
+from twod.wsgi.middleware import RoutingArgsMiddleware
+
+
+class TestRoutingArgs(object):
+    """Tests for the wsgiorg.routing_args middleware."""
+    
+    def setup(self):
+        self.mw = RoutingArgsMiddleware()
+        self.request = MockRequest({})
+    
+    def test_arguments_are_stored(self):
+        """The positional and named arguments should be stored in the environ"""
+        args = ("foo", "bar")
+        kwargs = {'arg': "value"}
+        self.mw.process_view(self.request, MOCK_VIEW, args, kwargs)
+        ok_("wsgiorg.routing_args" in self.request.environ)
+        eq_(len(self.request.environ['wsgiorg.routing_args']), 2)
+        eq_(self.request.environ['wsgiorg.routing_args'][0], args)
+        eq_(self.request.environ['wsgiorg.routing_args'][1], kwargs)
+    
+    def test_named_arguments_are_copied(self):
+        """
+        The named arguments must be copied, not passed by reference.
+        
+        Because Django will pass them on without inspecting the arguments for
+        the view, unlike other frameworks like Pylons.
+        
+        """
+        kwargs = {'arg': "value"}
+        self.mw.process_view(self.request, MOCK_VIEW, ("foo", "bar"), kwargs)
+        self.request.environ['wsgiorg.routing_args'][1]['new_arg'] = "new_value"
+        # The original dictionary must have not been modified:
+        eq_(len(kwargs), 1)
+        ok_("arg" in kwargs)
+    
+    def test_no_response(self):
+        """A response should never be returned."""
+        args = ("foo", "bar")
+        kwargs = {'arg': "value"}
+        result = self.mw.process_view(self.request, MOCK_VIEW, args, kwargs)
+        ok_(result is None)
+
+
+#{ Mock objects
+
+
+MOCK_VIEW = lambda request: "response"
+
+
+class MockRequest(object):
+    """
+    Mock Django request class.
+    
+    This way we won't need to set the DJANGO_SETTINGS_MODULE.
+    
+    """
+    
+    def __init__(self, environ):
+        self.environ = environ
+
+
+#}
+
--- twod/wsgi/middleware.py
+++ twod/wsgi/middleware.py
@@ -15,8 +15,20 @@
 #
 ##############################################################################
 """
-Enhanced WSGI support for Django applications.
+WSGI and Django middleware.
 
 """
 
+__all__ = ("RoutingArgsMiddleware", )
+
+
+class RoutingArgsMiddleware(object):
+    """
+    Django middleware which implements the `wsgiorg.routing_args standard
+    <http://wsgi.org/wsgi/Specifications/routing_args>`_.
+    
+    """
+    
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        request.environ['wsgiorg.routing_args'] = (view_args, view_kwargs.copy())
 
