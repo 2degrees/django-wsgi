5d73cf69276ddf832a916676791892bffa600ea1
Gustavo Narea <me@gustavonarea.net>
2010-02-19 11:53:18 +0000

    [docs] Added Ohloh badge

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,7 +94,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -134,7 +134,10 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+ohlog_sidebar = "ohloh-sidebar.html"
+sidebars = [ohlog_sidebar]
+html_sidebars = {'index': sidebars, 'manual/index': sidebars, 'api/index': sidebars,
+                 'about': sidebars, 'changelog': sidebars}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
