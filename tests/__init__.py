8953fe953fb70f700c7e9c1e1269967706f67bdb
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2015-03-19 16:46:27 +0000

    Set DJANGO_SETTINGS_MODULE at import time

--- tests/__init__.py
+++ tests/__init__.py
@@ -20,27 +20,26 @@ Test suite for :mod:`twod.wsgi`.
 from StringIO import StringIO
 import os
 
-import django.conf
+from django import conf
+from django.conf import LazySettings
+
+
+_DJANGO_SETTINGS_MODULE = "tests.dummy_django_project.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = _DJANGO_SETTINGS_MODULE
 
 
 class BaseDjangoTestCase(object):
-    """
-    Base test case to set up and reset the Django settings object.
-    
-    """
-    
-    django_settings_module = "tests.dummy_django_project.settings"
-    
-    setup_fixture = True
-    
+
     def setup(self):
-        if self.setup_fixture:
-            os.environ['DJANGO_SETTINGS_MODULE'] = self.django_settings_module
-    
+        self.__reset_settings()
+
     def teardown(self):
-        django.conf.settings = django.conf.LazySettings()
-        if "DJANGO_SETTINGS_MODULE" in os.environ:
-            del os.environ['DJANGO_SETTINGS_MODULE']
+        self.__reset_settings()
+
+    @staticmethod
+    def __reset_settings():
+        os.environ['DJANGO_SETTINGS_MODULE'] = _DJANGO_SETTINGS_MODULE
+        conf.settings = LazySettings()
 
 
 #{ Mock WSGI apps
@@ -51,7 +50,7 @@ class MockApp(object):
     Mock WSGI application.
     
     """
-    
+
     def __init__(self, status, headers):
         self.status = status
         self.headers = headers
@@ -83,11 +82,11 @@ class MockWriteApp(MockApp):
     Mock WSGI app which uses the write() function.
     
     """
-    
+
     def __call__(self, environ, start_response):
         self.environ = environ
         write = start_response(self.status, self.headers)
-        write( "body")
+        write("body")
         write(" as")
         write(" iterable")
         return []
@@ -95,24 +94,24 @@ class MockWriteApp(MockApp):
 
 class MockClosingApp(MockApp):
     """Mock WSGI app whose response contains a close() method."""
-    
+
     def __init__(self, *args, **kwargs):
         super(MockClosingApp, self).__init__(*args, **kwargs)
         self.app_iter = ClosingAppIter()
-    
+
     def __call__(self, environ, start_response):
-        body = super(MockClosingApp, self).__call__(environ,start_response)
+        body = super(MockClosingApp, self).__call__(environ, start_response)
         self.app_iter.extend(body)
         return self.app_iter
 
 
 class ClosingAppIter(list):
     """Mock response iterable with a close() method."""
-    
+
     def __init__(self, *args, **kwargs):
         super(ClosingAppIter, self).__init__(*args, **kwargs)
         self.closed = False
-    
+
     def close(self):
         self.closed = True
 
