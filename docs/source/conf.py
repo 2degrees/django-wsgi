a1dadf9bd04d7748ee34e639e362de886a4808ad
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2015-11-27 12:01:52 +0000

    Updated URL to WebOb docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -204,7 +204,7 @@ latex_documents = [
 intersphinx_mapping = {
     'http://docs.python.org/': None,
     'http://pythonpaste.org/': None,
-    'http://pythonpaste.org/webob/': None,
+    'http://docs.webob.org/en/latest/': None,
     'http://pythonpaste.org/webtest/': None,
     'http://pythonpaste.org/deploy/': None,
     'http://docs.djangoproject.com/en/dev/': "http://docs.djangoproject.com/en/dev/_objects/",
